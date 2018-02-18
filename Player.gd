6da32d2a8f0d80ccc190bed67171e584762fd9b7
Fisent <lukzieniewicz@gmail.com>
2018-02-18 22:14:25 +0100

    Moving and jumping done

--- Player.gd
+++ Player.gd
@@ -1,15 +1,22 @@
 extends KinematicBody2D
 
+const GRAVITY = 20
+const SPEED = 400
+const JUMP_HEIGHT = -500
+const UP = Vector2(0,-1)
 var motion = Vector2()
 
 func _physics_process(delta):
 	if Input.is_action_pressed('ui_right'):
-		motion.x = 200
-	if Input.is_action_pressed('ui_left'):
-		motion.x = -200
-	if Input.is_action_pressed('ui_up'):
-		motion.y = 200
-	if Input.is_action_pressed('ui_down'):
-		motion.y = -200
+		motion.x = SPEED
+	elif Input.is_action_pressed('ui_left'):
+		motion.x = -SPEED
+	else:
+		motion.x = 0
+		
+	motion.y += GRAVITY
 	
-	move_and_slide(motion)
+	if is_on_floor():
+		if Input.is_action_pressed('ui_up'):
+			motion.y = JUMP_HEIGHT
+	motion = move_and_slide(motion, UP)
